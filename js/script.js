a6195f0b50f664c35c566da96a3045c7df1c2135
mizanurrahman70 <mizanurrahmanofficial70.com>
2024-02-15 08:12:52 +0600

    added my code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-console.log('connecteddd')
+// main function play btn
+function getsPlayGame(){
+   playGamePage('play-alpha');
+   homePage('home');
+   starting()
+}
+
+// play game secton 
+function playGamePage(play){
+    const continu =document.getElementById(play);
+    continu.classList.remove('hidden')
+}
+
+// first page hiden 
+function homePage(page){
+const home=document.getElementById(page);
+home.classList.add('hidden')
+}
+// continue game 
+function starting(){
+    const alphabet=getAlphabet()
+    console.log('alphaber', alphabet)
+}
+
+
+// get alphabet
+function getAlphabet(){
+    const alphabetSteing='abcdefghijkllmnopdqrstuvwxyz'
+   const alphabets= alphabetSteing.split('')
+//    get random Number
+const randomNumber=Math.random()*25;
+const Number=Math.round(randomNumber)
+const alphabet =alphabets[Number]
+return alphabet
+}
+
